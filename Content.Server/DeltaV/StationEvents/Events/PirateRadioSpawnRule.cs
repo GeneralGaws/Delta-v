0016c27613c3fec3e0ccf91728d05cd5abc80e1f
OCO_Omega <42233446+OCOtheOmega@users.noreply.github.com>
2024-02-02 07:27:14 +0300

    Listening post map changes (#156)
    * updates map, lowers server volume
    
    * Delete PLEASEWORKTHISTIMEIBEGYOUS14.yml
    
    * Update DV-pirateradio.yml
    
    * changes fax machine to *not* alert admins; and start with emagged by default instead of using VV
    
    * Revert "changes fax machine to *not* alert admins; and start with emagged by default instead of using VV"
    
    This reverts commit 05bb5f402d7986214f753dd217765e01b46e69a7.
    
    * Revert "Update DV-pirateradio.yml"
    
    This reverts commit e77fb34e86c994bce2f63205741636be6c0286fd.
    
    * Revert "Delete PLEASEWORKTHISTIMEIBEGYOUS14.yml"
    
    This reverts commit 2000e2c147125827405eeaf1191abb29c7b4c7d8.
    
    * Revert "updates map, lowers server volume"
    
    This reverts commit d99cab6f36c26441c8431ee5c7d2fc2ad1ff68d7.
    
    * adds new syndicate fax, maps it in as well as other QOL changes
    
    * adds cybersun pen to Syndicate PDA's
    
    * Update DV-pirateradio.yml
    
    * adds blacklist to CC Fax + quieter server
    
    * Update PirateRadioSpawnRule.cs
    
    * unfinished scuttling system
    
    * Adding colorblind quality of life.
    
    * Tweaks to map and guards, no more self destruct system
    
    * added guard flavor text
    
    * The reasons for these are now gone
    
    * DONE
    
    * Update events.yml
    
    * Update PirateRadioSpawnRule.cs
    
    * Update DV-pirateradio.yml
    
    * fixes crew monitor server not working at *extreme* ranges
    
    * adds delta-v comment where needed
    
    * Small code optimization and tons of documentation
    
    * Update outpost visuals! Guards have a PDA now
    
    * Update DV-pirateradio.yml
    
    * Update DV-pirateradio.yml
    
    * Goodbye crab. Hello Content.Shared migration
    
    * Cleaning up pirateradio spawn
    
    * Update DV-pirateradio.yml
    
    * reverts pen changes to upstream PDA's
    
    * adds new PDA
    
    * adds new PDA to loadouts.
    
    * Update Resources/Prototypes/DeltaV/Entities/Mobs/NPCs/salvage.yml
    
    Co-authored-by: Debug <49997488+DebugOk@users.noreply.github.com>
    Signed-off-by: Danger Revolution! <142105406+DangerRevolution@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: VMSolidus <evilexecutive@gmail.com>
    Signed-off-by: Danger Revolution! <142105406+DangerRevolution@users.noreply.github.com>
    Co-authored-by: DangerRevoltion <142105406+DangerRevoltion@users.noreply.github.com>
    Co-authored-by: Danger Revolution! <142105406+DangerRevolution@users.noreply.github.com>
    Co-authored-by: VMSolidus <evilexecutive@gmail.com>
    Co-authored-by: Debug <49997488+DebugOk@users.noreply.github.com>

--- Content.Server/DeltaV/StationEvents/Events/PirateRadioSpawnRule.cs
+++ Content.Server/DeltaV/StationEvents/Events/PirateRadioSpawnRule.cs
@@ -1,32 +1,91 @@
 using Robust.Server.GameObjects;
 using Robust.Server.Maps;
 using Robust.Shared.Map;
+using Robust.Shared.Map.Components;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Random;
+using Robust.Shared.Utility;
 using Content.Server.GameTicking;
-using Content.Server.GameTicking.Rules;
 using Content.Server.GameTicking.Rules.Components;
 using Content.Server.StationEvents.Components;
-using Content.Server.RoundEnd;
+using Content.Server.Station.Components;
+using Content.Shared.Salvage;
+using Content.Shared.Random.Helpers;
+using System.Linq;
 
 namespace Content.Server.StationEvents.Events;
 
 public sealed class PirateRadioSpawnRule : StationEventSystem<PirateRadioSpawnRuleComponent>
 {
+    [Dependency] private readonly IEntityManager _entities = default!;
     [Dependency] private readonly IMapManager _mapManager = default!;
     [Dependency] private readonly MapLoaderSystem _map = default!;
-    [Dependency] private readonly GameTicker _gameTicker = default!;
-    [Dependency] private readonly TraitorRuleSystem _TraitorRuleSystem = default!;
+    [Dependency] private readonly IRobustRandom _random = default!;
+    [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
 
     protected override void Started(EntityUid uid, PirateRadioSpawnRuleComponent component, GameRuleComponent gameRule, GameRuleStartedEvent args)
     {
+        //This handles spawning the Listening Outpost
         base.Started(uid, component, gameRule, args);
-		
-        var shuttleMap = _mapManager.CreateMap();
-        var options = new MapLoadOptions
-        {
-            LoadMap = true,
-        };
 
-        _map.TryLoad(shuttleMap, component.PirateRadioShuttlePath, out _, options);
+        var xformQuery = GetEntityQuery<TransformComponent>();
+        //Find where the station is and get a bounding box
+        var aabbs = EntityQuery<StationDataComponent>().SelectMany(x =>
+                x.Grids.Select(x =>
+                    xformQuery.GetComponent(x).WorldMatrix.TransformBox(_mapManager.GetGridComp(x).LocalAABB)))
+            .ToArray();
+        if (aabbs.Length < 1) return;
+        var aabb = aabbs[0];
+
+        for (var i = 1; i < aabbs.Length; i++)
+        {
+            aabb.Union(aabbs[i]);
+        }
+        //Generates a potential spawning area, shaped like a square with rounded corners.
+        //DistanceModifier allows for fine tuning of how large this area is.
+        //The initial target distance(for DistanceModifier = 20f) is between 1km and 1.5km of the station.
+        //But with the way NextVector2 works, it can be less than that
+        //Do not for any reason set DistanceModifier greater than 25f or less than 1f. 
+        var a = MathF.Max(aabb.Height / 2f, aabb.Width / 2f) * component.DistanceModifier;
+        var randomoffset = _random.NextVector2(a, a * 2.5f);
+        var outpostOptions = new MapLoadOptions
+        {
+            Offset = aabb.Center + randomoffset,
+            LoadMap = false,
+        };
+        //Now spawn the Listening Outpost
+        _map.TryLoad(GameTicker.DefaultMap, component.PirateRadioShuttlePath, out var outpostids, outpostOptions);
+
+        //Now we generate the outpost's debris field
+        if (outpostids == null) return;
+        //Yes, this is a loop within a loop. Actually, foreach is just here to convert an array into a variable.
+        //For whatever ungodly reason, Outpostids is an array of gridUids, even though it can only ever contain a single gridUid. 
+        foreach (var id in outpostids)
+        {
+            if (!TryComp<MapGridComponent>(id, out var grid)) return;
+            //Obtain the bounding box of the Listening Outpost
+            var outpostaabb = _entities.GetComponent<TransformComponent>(id).WorldMatrix.TransformBox(grid.LocalAABB);
+            var b = MathF.Max(outpostaabb.Height / 2f, aabb.Width / 2f) * component.DebrisDistanceModifier; //DebrisDistanceModifier controls how dense we want the debris field to be
+            int k = 1;
+            while (k < component.DebrisCount + 1) //DebrisCount defines how many wrecks to spawn
+            {
+                //Generate the region to spawn the debris
+                //We have to remake this region for every wreck, otherwise they'll all spawn in the same place
+                var debrisRandomOffset = _random.NextVector2(b, b * 2.5f);
+                var randomer = _random.NextVector2(b, b * 5f); //Second random vector to ensure the outpost isn't perfectly centered in the debris field
+                var debrisOptions = new MapLoadOptions
+                {
+                    Offset = outpostaabb.Center + debrisRandomOffset + randomer,
+                    LoadMap = false,
+                };
+
+
+                var salvageProto = _random.Pick(_prototypeManager.EnumeratePrototypes<SalvageMapPrototype>().ToList());
+                //And finally spawn a salvage wreck
+                _map.TryLoad(GameTicker.DefaultMap, salvageProto.MapPath.ToString(), out _, debrisOptions);
+                k++;
+            }
+        }
     }
 
     protected override void Ended(EntityUid uid, PirateRadioSpawnRuleComponent component, GameRuleComponent gameRule, GameRuleEndedEvent args)
