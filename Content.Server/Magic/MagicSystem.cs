2bb1e54c5532ed3b2e29e5fb82054bbe2e859d00
GeneralGaws <limonmessi@mail.ru>
2024-04-07 16:22:51 +0300

    ок

--- Content.Server/Magic/MagicSystem.cs
+++ Content.Server/Magic/MagicSystem.cs
@@ -25,7 +25,26 @@ using Robust.Shared.Map.Components;
 using Robust.Shared.Random;
 using Robust.Shared.Serialization.Manager;
 using Robust.Shared.Spawners;
-
+        //Stray
+using Content.Server.Emp;
+using Content.Server.Polymorph.Systems;
+using Content.Shared.Polymorph;
+using Content.Server.Fluids.EntitySystems;
+using System.Diagnostics;
+using Content.Shared.Mobs.Components;
+using Content.Shared.Mobs.Systems;
+using Content.Server.NPC.HTN;
+using Content.Shared.NPC.Systems;
+using Content.Shared.Eye.Blinding.Systems;
+using System.Threading.Tasks;
+using Content.Shared.Eye.Blinding.Components;
+using Content.Shared.Explosion.Components;
+using System.Threading;
+using Content.Server.Explosion.EntitySystems;
+using System.Transactions;
+using Content.Server.Mind;
+using Content.Shared.StatusEffect;
+        //Stray
 namespace Content.Server.Magic;
 
 /// <summary>
@@ -48,7 +67,19 @@ public sealed class MagicSystem : EntitySystem
     [Dependency] private readonly SharedAudioSystem _audio = default!;
     [Dependency] private readonly ChatSystem _chat = default!;
     [Dependency] private readonly ActionContainerSystem _actionContainer = default!;
-
+    //Stray
+    [Dependency] private readonly EmpSystem _emp = default!;
+    [Dependency] private readonly PolymorphSystem _polymorph = default!;
+    [Dependency] private readonly SmokeSystem _smoke = default!;
+    [Dependency] private readonly EntityManager _entityManager = default!;
+    [Dependency] private readonly MobStateSystem _mobStateSystem = default!;
+    [Dependency] private readonly NpcFactionSystem _npcFactionSystem = default!;
+    [Dependency] private readonly MetaDataSystem _metaData = default!;
+    [Dependency] private readonly BlindableSystem _blindnessSystem = default!;
+    [Dependency] private readonly ExplosionSystem _explosionSystem = default!;
+    [Dependency] private readonly MindSystem _mind = default!;
+    [Dependency] private readonly StatusEffectsSystem _statusEffects = default!;
+    //Stray
     public override void Initialize()
     {
         base.Initialize();
@@ -64,6 +95,16 @@ public sealed class MagicSystem : EntitySystem
         SubscribeLocalEvent<WorldSpawnSpellEvent>(OnWorldSpawn);
         SubscribeLocalEvent<ProjectileSpellEvent>(OnProjectileSpell);
         SubscribeLocalEvent<ChangeComponentsSpellEvent>(OnChangeComponentsSpell);
+        //Stray
+        SubscribeLocalEvent<EMPSpellEvent>(OnEMPSpell);
+        SubscribeLocalEvent<DevolveSpellEvent>(OnDevolveSpell);
+        SubscribeLocalEvent<AetherFormSpellEvent>(OnAetherFormSpell);
+        SubscribeLocalEvent<HulkFormSpellEvent>(OnHulkFormSpell);
+        SubscribeLocalEvent<SwapSpellEvent>(OnSwapSpell);
+        SubscribeLocalEvent<AnimateDeadSpellEvent>(OnAnimateDead);
+        SubscribeLocalEvent<BlindSpellEvent>(OnBlindSpell);
+        SubscribeLocalEvent<SmokeSpellEvent>(OnSmokeSpell);
+        //Stray
     }
 
     private void OnDoAfter(EntityUid uid, SpellbookComponent component, DoAfterEvent args)
@@ -87,6 +128,10 @@ public sealed class MagicSystem : EntitySystem
         }
 
         component.SpellActions.Clear();
+        if (component.DeleteAfterLearn)
+        {
+            _entityManager.DeleteEntity(uid);
+        }
     }
 
     private void OnInit(EntityUid uid, SpellbookComponent component, MapInitEvent args)
@@ -393,6 +438,169 @@ public sealed class MagicSystem : EntitySystem
             }
         }
     }
+
+    //Stray
+
+        private void OnEMPSpell(EMPSpellEvent args)
+    {
+        if (args.Handled)
+            return;
+
+        args.Handled = true;
+        Speak(args);
+
+        //Get the position of the player
+        var transform = Transform(args.Performer);
+        var coords = transform.MapPosition;
+
+        _audio.PlayPvs(args.EmpSound, args.Performer, AudioParams.Default.WithVolume(args.EmpVolume));
+
+        _emp.EmpPulse(coords, 4f, 50000, 10f);
+
+    }
+
+
+    public void OnDevolveSpell(DevolveSpellEvent ev)
+    {
+        if (ev.Handled)
+            return;
+
+        ev.Handled = true;
+        Speak(ev);
+
+        _polymorph.PolymorphEntity(ev.Target, "ArtifactMonkey");
+    }
+
+    public void OnAetherFormSpell(AetherFormSpellEvent ev)
+    {
+        if (ev.Handled)
+            return;
+
+        ev.Handled = true;
+        Speak(ev);
+
+        _polymorph.PolymorphEntity(ev.Performer, "WizardAetherForm");
+    }
+
+
+    public void OnHulkFormSpell(HulkFormSpellEvent args)
+    {
+        if (args.Handled)
+            return;
+
+        args.Handled = true;
+        Speak(args);
+
+
+        var transform = Transform(args.Performer);
+        if (!_mapManager.TryFindGridAt(transform.MapPosition, out _, out var grid))
+            return;
+
+        var coords = grid.MapToGrid(transform.MapPosition);
+
+        _audio.PlayPvs(args.SmokeSound, args.Performer, AudioParams.Default.WithVolume(-3f));
+        var ent = Spawn("Smoke", coords.SnapToGrid());
+        _smoke.StartSmoke(ent, new(), 5, 5);
+
+
+        _audio.PlayPvs(args.HulkScream, args.Performer, AudioParams.Default.WithVolume(5f));
+
+        _polymorph.PolymorphEntity(args.Performer, "WizardHulkForm");
+    }
+
+    private void OnAnimateDead(AnimateDeadSpellEvent args)
+    {
+        {
+            if (args.Handled)
+                return;
+
+            args.Handled = true;
+            Speak(args);
+
+            var transform = Transform(args.Performer);
+            var coords = transform.Coordinates;
+
+            _audio.PlayPvs(args.AnimateSound, args.Performer, AudioParams.Default.WithVolume(args.AnimateVolume));
+            foreach (var entity in _lookup.GetEntitiesInRange(coords, args.Range))
+            {
+                if (!TryComp<MobStateComponent>(entity, out var mobState))
+                    continue;
+                if (_mobStateSystem.IsDead(entity))
+                {
+                    if (!TryComp<BodyComponent>(entity, out var body))
+                        return;
+                    _bodySystem.GibBody(entity, true, body);
+                    var mobtransform = Transform(entity);
+                    var mobcoords = mobtransform.MapPosition;
+                    var mob = Spawn("MobSkeletonAngry", mobcoords);
+                    var comp = _entityManager.AddComponent<HTNComponent>(mob);
+                    comp.RootTask = new HTNCompoundTask()
+                    {
+                        Task = "SimpleHostileCompound"
+                    };
+                }
+            }
+        }
+    }
+
+    private async void OnBlindSpell(BlindSpellEvent ev)
+    {
+        {
+            if (ev.Handled)
+                return;
+
+            ev.Handled = true;
+            Speak(ev);
+            var comp = Comp<BlindableComponent>(ev.Target);
+            var olddamage = comp.EyeDamage;
+            _blindnessSystem.AdjustEyeDamage(ev.Target, 9);
+            _blindnessSystem.UpdateIsBlind(ev.Target);
+            await Task.Delay(10000);
+            _blindnessSystem.AdjustEyeDamage(ev.Target, -(9 - olddamage));
+            _blindnessSystem.UpdateIsBlind(ev.Target);
+        }
+    }
+
+    public void OnSwapSpell(SwapSpellEvent ev)
+    {
+        if (ev.Handled)
+            return;
+
+        ev.Handled = true;
+        Speak(ev);
+
+        var transform = Transform(ev.Performer);
+        var coords = transform.Coordinates;
+
+        var transformother = Transform(ev.Target);
+        var coordsother = transformother.Coordinates;
+
+        _transformSystem.SetCoordinates(ev.Performer, coordsother);
+        _transformSystem.SetCoordinates(ev.Target, coords);
+    }
+
+    private void OnSmokeSpell(SmokeSpellEvent args)
+    {
+        if (args.Handled)
+            return;
+
+        args.Handled = true;
+        Speak(args);
+
+        //Get the position of the player
+        var transform = Transform(args.Performer);
+        if (!_mapManager.TryFindGridAt(transform.MapPosition, out _, out var grid))
+            return;
+        // var coords = transform.MapPosition;
+        var coords = grid.MapToGrid(transform.MapPosition);
+
+        _audio.PlayPvs(args.SmokeSound, args.Performer, AudioParams.Default.WithVolume(args.SmokeVolume));
+        var ent = Spawn("Smoke", coords.SnapToGrid());
+        _smoke.StartSmoke(ent, new(), 15, 40);
+    }
+
+    //Stray
+
 
     #endregion
 
