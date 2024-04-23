8e5d180394ead3a7d235a4f7ebb4c6e43a998078
OCO_Omega <42233446+OCOtheOmega@users.noreply.github.com>
2024-04-23 11:50:19 +0300

    fix

--- Content.Server/DetailExaminable/DetailExaminableComponent.cs
+++ Content.Server/DetailExaminable/DetailExaminableComponent.cs
@@ -1,11 +1,9 @@
-﻿// Parkstation-CharacterInformation-Start - This was moved to the Shared namespace
-// namespace Content.Server.DetailExaminable
-// {
-//     [RegisterComponent]
-//     public sealed class DetailExaminableComponent : Component
-//     {
-//         [DataField("content", required: true)] [ViewVariables(VVAccess.ReadWrite)]
-//         public string Content = "";
-//     }
-// }
-// Parkstation-CharacterInformation-End
+﻿namespace Content.Server.DetailExaminable
+{
+    [RegisterComponent]
+    public sealed partial class DetailExaminableComponent : Component
+    {
+        [DataField("content", required: true)] [ViewVariables(VVAccess.ReadWrite)]
+        public string Content = "";
+    }
+}
--- Content.Shared/SimpleStation14/Examine/CharacterInformation/Components/CharacterInformationComponent.cs
+++
@@ -1,9 +0,0 @@
-using Robust.Shared.GameStates;
-
-namespace Content.Shared.SimpleStation14.Examine.CharacterInformation.Components;
-
-[RegisterComponent, NetworkedComponent]
-public sealed partial class CharacterInformationComponent : Component
-{
-
-}
